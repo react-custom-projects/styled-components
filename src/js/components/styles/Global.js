a207a25fb60058ed6271da56b8a1133bddcdcc7d
Adam Morsi <adam.morsi@yahoo.com>
2021-11-23 15:30:13 +0200

    destructure theme

--- src/js/components/styles/Global.js
+++ src/js/components/styles/Global.js
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        background-color: #ffffff;
+        background-color: ${({ theme: { colors } }) => colors.body};
         color: hsl(192, 100%, 9%);
         font-family: 'Poppins', sans-serif;
         font-size: 1.15em;
--- src/js/components/styles/Header.styled.js
+++ src/js/components/styles/Header.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-	background-color: ${({ theme }) => theme.colors.header};
+	background-color: ${({ theme: { colors } }) => colors.header};
 	padding: 40px 0;
 `;
