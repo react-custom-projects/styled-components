1541e6f4c66f1d6f27f50214c801d02fc43336e0
Adam Morsi <adam.morsi@yahoo.com>
2021-11-23 15:53:31 +0200

    create the nav

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 //styles
-import { StyledHeader } from './styles/Header.styled';
+import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { Button } from './styles/Button.styled';
+import { Container } from './styles/Container.styled';
+//images
+import logoImg from '../../assets/images/logo.svg';
 
 const Header = () => {
 	return (
 		<StyledHeader>
-			<h1>Hubble</h1>
+			<Container>
+				<Nav>
+					<Logo src={logoImg} alt="logo" />
+					<Button>Try it free</Button>
+				</Nav>
+			</Container>
 		</StyledHeader>
 	);
 };
--- src/js/components/styles/Header.styled.js
+++ src/js/components/styles/Header.styled.js
@@ -4,3 +4,12 @@ export const StyledHeader = styled.header`
 	background-color: ${({ theme: { colors } }) => colors.header};
 	padding: 40px 0;
 `;
+
+export const Nav = styled.nav`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 40ox;
+`;
+
+export const Logo = styled.img``;
